f03ef36ded016b9b687c33aa41212585287f3e15
VT55555 <vasyatsap1988@gmail.com>
2026-09-18 21:21:34 +0200

    Implement HTTP server to indicate bot status
    Added a simple HTTP server that responds with a message.

--- bot.py
+++ bot.py
@@ -3,7 +3,22 @@ import threading
 import telebot
 import ccxt
 import pandas as pd
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
 
+def run_server():
+    class SimpleHandler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Bot is running!")
+    
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), SimpleHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_server, daemon=True).start()2
 # === ВАШІ ДАНІ ЗБЕРЕЖЕНО ===
 TOKEN ='8563940267:AAGoGY8KsJqhlLHaUuj5UIMPPKQj65-Snys'
 CHAT_ID = 5506822047
